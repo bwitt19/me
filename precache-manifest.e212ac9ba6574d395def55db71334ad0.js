f9befbc6b766f3ee86cce861d1908b3b364448ab
Bea Witt <bwitt190@gmail.com>
2020-07-30 22:31:22 -0400

    Updates

---
+++ precache-manifest.e212ac9ba6574d395def55db71334ad0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113f7a3f8d8a995791c04c236b1deebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e059b93e5c0673eddeb0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3949c87f8bd2bdca35a0",
+    "url": "/static/css/main.482151e0.chunk.css"
+  },
+  {
+    "revision": "e059b93e5c0673eddeb0",
+    "url": "/static/js/2.4485c28f.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.4485c28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3949c87f8bd2bdca35a0",
+    "url": "/static/js/main.7d5f42f8.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "21e8372fb1df9965eb6df9073c2c6336",
+    "url": "/static/media/itme.21e8372f.jpg"
+  }
+]);
