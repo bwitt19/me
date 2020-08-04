49079d951d8f54593fdfc95d0f55ffff8b9ab184
Bea Witt <bwitt190@gmail.com>
2020-08-04 19:09:37 -0400

    Updated references to resume

---
+++ src/components/data/resume_site.js
@@ -0,0 +1,3 @@
+const resume_site = "https://raw.githubusercontent.com/bwitt19/me/41f43a69bd0a5344bc3583bbdca08d6df140d7de/src/components/data/Blair%20Witt%20Resume%20Jul-20%20online%20ver.pdf";
+
+export default resume_site;
