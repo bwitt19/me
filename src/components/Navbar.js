dedb9717bb06c029882939c4f0a0977a3a835d7e
Bea Witt <bwitt190@gmail.com>
2020-08-04 18:05:08 -0400

    Navbar given proper resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Container } from "react-bootstrap"
+
+const resume_site = "https://raw.githubusercontent.com/bwitt19/me/32c12b603236132ec8de95da96069fe03b72aa3b/src/components/data/Blair%20Witt%20Resume%20Jul-20.pdf";
 
 function Navbar() {
     return(
         <Container className="my-4" id="navbar" fluid>
             <nav>
                 <a href='#about'>About</a> &bull;&nbsp;
-                <a href='#resume'>Resume</a> &bull;&nbsp;
                 <a href='#experience'>Experience</a> &bull;&nbsp;
-                <a href='#projects'>Projects</a>
+                <a href='#projects'>Projects</a> &bull;&nbsp;
+                <a href={resume_site}>Resume</a> 
             </nav>
         </Container>
     );
