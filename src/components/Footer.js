7881a0857c8130de6d7b65dfb2d23bca860fc756
Bea Witt <bwitt190@gmail.com>
2020-08-04 18:07:48 -0400

    Footer links updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { Container } from 'react-bootstrap';
 
+const resume_site = "https://raw.githubusercontent.com/bwitt19/me/32c12b603236132ec8de95da96069fe03b72aa3b/src/components/data/Blair%20Witt%20Resume%20Jul-20.pdf";
+
+
 class Footer extends Component {
     render() {
         return (
@@ -9,7 +12,7 @@ class Footer extends Component {
                     <nav>
                         <a href="mailto:bwitt190@gmail.com">[email]</a> &bull;&nbsp;
                         <a href="https://github.com/bwitt19">[github]</a> &bull;&nbsp;
-                        <a href="#oops">full resume</a>
+                        <a href={resume_site}>full resume</a>
                     </nav>
                     <div>
                         <a href="https://github.com/bwitt19/bwitt19.github.io" title="full repository here!">made from scratch</a> with ReactJS and Bootstrap.
