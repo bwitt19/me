f608993990a7bea4b9e3296303324bdcc391309a
Bea Witt <bwitt190@gmail.com>
2020-08-10 22:09:14 -0400

    Publication section styled

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,7 @@ code is just for the interface.
 const pubs_markdown = `
 # publications:
 
-[[1]](https://ieeexplore.ieee.org/abstract/document/8730692): K. Rook, B. Witt, R. Bailey, J. Geigel, P. Hu and A. Kothari, "A Study of User Intent in Immersive Smart Spaces," 2019 IEEE International Conference on Pervasive Computing and Communications Workshops (PerCom Workshops), Kyoto, Japan, 2019, pp. 227-232, doi: 10.1109/PERCOMW.2019.8730692.
+[[1]](https://ieeexplore.ieee.org/abstract/document/8730692): K. Rook, B. Witt, R. Bailey, J. Geigel, P. Hu and A. Kothari, "A Study of User Intent in Immersive Smart Spaces," *2019 IEEE International Conference on Pervasive Computing and Communications Workshops (PerCom Workshops)*, Kyoto, Japan, 2019, pp. 227-232, doi: 10.1109/PERCOMW.2019.8730692.
 `
 
 class Projects extends Component {
